42624b21b99dcc396896c397281a9029d82527bb
Ullhas Arunkumar <ullhasarunkumar@gmail.com>
2022-10-12 10:50:19 +0530

    first commit

---
+++ Source.java
@@ -0,0 +1,10 @@
+
+public class Source {
+
+   public static void main(String[] args) {
+
+       //initialize your variable here
+    int firstVariable=100;
+       System.out.println(firstVariable);
+   }
+}
